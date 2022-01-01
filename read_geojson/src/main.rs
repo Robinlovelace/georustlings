49a50f5b837e69e5c5460aa142c9fec607bae2d4
Robin Lovelace <rob00x@gmail.com>
2022-01-01 18:58:02 +0000

    add serde_json

--- read_geojson/src/main.rs
+++ read_geojson/src/main.rs
@@ -1,9 +1,13 @@
 use geojson::GeoJson;
+use serde_json::from_str;
 
 fn main() {
     let geojson_path = "../data/geojson/countries.geojson";
     let geojson_input = std::fs::read_to_string(geojson_path).unwrap();
+    let geojson: GeoJson = serde_json::from_str(&geojson_input);
+    let features = geojson.features();
+    println!("{:?}", geojson.into_iter().count());
+    // let features = geojson.features();
     let slice = &geojson_input[..500];
     println!("{}", slice);
-    // println!("{:?}", zones);
 }
